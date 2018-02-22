692a933a3044ffd66e2ded01794d10b5aa2d558f
Gabriel Lumbi <gabriel.lumbi>
2018-02-22 22:02:43 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: "./src/main.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /pixi\.js$/, use: "expose-loader?PIXI" },
+      { test: /p2\.js$/, use: "expose-loader?p2" },
+      { test: /phaser\.js$/, use: "expose-loader?Phaser" },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({template: 'src/index.html'})]
+};
+
+module.exports = config
